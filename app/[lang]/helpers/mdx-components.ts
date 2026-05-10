0eb270924355ea6dc04f4be364c0e9675ce2dc26
Santana <whimsicaltech@proton.me>
2026-05-10 09:53:01 +0900

    removed unused blog image component

--- app/[lang]/helpers/mdx-components.ts
+++ app/[lang]/helpers/mdx-components.ts
@@ -1,5 +1,4 @@
 import CodeSnippet from "../components/CodeSnippet/CodeSnippet";
-import ImageComponent from "../components/BlogImageComponent/BlogImageComponent";
 
 const COMPONENT_MAP = {
   pre: CodeSnippet,
