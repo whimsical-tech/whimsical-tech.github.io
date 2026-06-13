e1aa732bc72ffe5235c13f8f1ebcf46ecdfb06dc
Santana <whimsicaltech@proton.me>
2026-06-13 10:32:11 +0900

    added qiita and linkedin links to contact section

--- app/components/Contact/Contact.tsx
+++ app/components/Contact/Contact.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import { getDictionary } from "@/dictionaries";
 import { Locale, hasLocale } from "@/i18n-config";
+import Image from "next/image";
 import styles from "./Contact.module.css";
 
 export default async function Contact({
@@ -22,6 +23,12 @@ export default async function Contact({
           whimsicaltech+portfolio@proton.me
         </a>
       </p>
+      <a href="https://qiita.com/frontendpro" target="_blank" rel="noreferrer">
+        <Image src="/qiita.png" alt="Qiita" width={40} height={40} />
+      </a>
+      <a href="https://qiita.com/frontendpro" target="_blank" rel="noreferrer">
+        <Image src="/in.png" alt="LinkedIn" width={40} height={40} />
+      </a>
     </footer>
   );
 }
